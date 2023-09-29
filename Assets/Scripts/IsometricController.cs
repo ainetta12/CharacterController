77c35599257aa0cb01b9f6edc7cf9b760ff92a8e
ainetta12 <ainasanzg@gmail.com>
2023-09-29 09:30:48 +0200

    movimiento

--- Assets/Scripts/IsometricController.cs
+++ Assets/Scripts/IsometricController.cs
@@ -4,15 +4,30 @@ using UnityEngine;
 
 public class IsometricController : MonoBehaviour
 {
+    private CharacterController _controller;
+    private float _horizontal;
+    private float _vertical;
+
+    [SerializeField] private float playerSpeed = 5;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        _controller = GetComponent <CharacterController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        _horizontal = Input.GetAxisRaw("Horizontal");
+        _vertical =Input.GetAxisRaw("Vertical");
+
+        Movement();
+    }
+
+    void Movement()
+    {
+        Vector3 direction = new Vector3(_horizontal, 0, _vertical);
+        _controller.Move(direction * playerSpeed * Time.deltaTime);
     }
 }
