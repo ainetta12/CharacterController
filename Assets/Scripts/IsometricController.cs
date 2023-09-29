a9d79c7bfd4f3045ab65e15191e02fab591d8a4f
ainetta12 <ainasanzg@gmail.com>
2023-09-29 10:49:00 +0200

    jump

--- Assets/Scripts/IsometricController.cs
+++ Assets/Scripts/IsometricController.cs
@@ -8,7 +8,27 @@ public class IsometricController : MonoBehaviour
     private float _horizontal;
     private float _vertical;
 
-    [SerializeField] private float playerSpeed = 5;
+
+    //variable para saltar y gravedad
+
+    [SerializeField] private float playerSpeed = 5; 
+    [SerializeField] private float _jumpHeight = 1;
+
+    private float _gravity = -9.81f;
+    private Vector3 _playerGravity;
+
+    //Variables para rotacion
+
+     private float turnSmoothVeloity;
+
+    [SerializeField] float turnSmoothTime = 0.1f;
+
+    //variable para sensor
+
+    [SerializeField] private Transform _sensorPosition;
+    [SerializeField] private float _sensorRadius = 0.2f;
+    [SerializeField] private LayerMask _groundLayer;
+    private bool _isGrounded;
 
     // Start is called before the first frame update
     void Start()
@@ -23,6 +43,7 @@ public class IsometricController : MonoBehaviour
         _vertical =Input.GetAxisRaw("Vertical");
 
         Movement();
+        Jump();
     }
 
     void Movement()
@@ -32,12 +53,33 @@ public class IsometricController : MonoBehaviour
       if(direction !=  Vector3.zero)
         {
             float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
+            float smoothAngle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVeloity, turnSmoothTime);
 
-            transform.rotation = Quaternion.Euler(0, targetAngle, 0);
+            transform.rotation = Quaternion.Euler(0, smoothAngle, 0);
 
             _controller.Move(direction * playerSpeed * Time.deltaTime);
         }
+ 
+    }
 
-        
+    void Jump()
+    {
+
+        _isGrounded = Physics.CheckSphere(_sensorPosition.position, _sensorRadius, _groundLayer);
+       
+        if(_isGrounded && _playerGravity.y < 0)
+        {
+            _playerGravity.y = 0;
+        }
+
+        if(_isGrounded && Input.GetButtonDown("Jump"))
+        {
+            _playerGravity.y = Mathf.Sqrt(_jumpHeight * -2 * _gravity);
+        }
+
+        _playerGravity.y += _gravity * Time.deltaTime;
+
+        _controller.Move(_playerGravity * Time.deltaTime);
+
     }
 }
